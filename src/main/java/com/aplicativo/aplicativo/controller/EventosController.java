4d8ea4a73d4b02736f792b2bdbf3161fcf0eb063
Gileno <gileno.cordeiroc@gmail.com>
2020-04-27 09:50:05 -0300

    Persistência no DB Adicionada

--- src/main/java/com/aplicativo/aplicativo/controller/EventosController.java
+++ src/main/java/com/aplicativo/aplicativo/controller/EventosController.java
@@ -1,14 +1,28 @@
 package com.aplicativo.aplicativo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aplicativo.aplicativo.eventoRepository.EventoRepository;
+import com.aplicativo.aplicativo.models.Eventos;
 
 @Controller
 public class EventosController {
 	
-	@RequestMapping("/cadastrarEvento")
-	public String eventoController() {
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
+	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Eventos evento) {
+		er.save(evento);// sava no datavbase
+		return "redirect:/cadastrarEvento";//redireciona para o evento do GET
 	}
 
 }
---
+++ src/main/java/com/aplicativo/aplicativo/eventoRepository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.aplicativo.aplicativo.eventoRepository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.aplicativo.aplicativo.models.Eventos;
+
+public interface EventoRepository extends CrudRepository<Eventos, String>{
+
+}
