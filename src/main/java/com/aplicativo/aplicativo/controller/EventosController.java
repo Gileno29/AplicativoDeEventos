4de2298a5f4b6a7eff371531cb6781ddd780e10d
Gileno <gileno.cordeiroc@gmail.com>
2020-04-27 15:43:59 -0300

    Adição do serialize

--- src/main/java/com/aplicativo/aplicativo/controller/EventosController.java
+++ src/main/java/com/aplicativo/aplicativo/controller/EventosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.aplicativo.aplicativo.eventoRepository.EventoRepository;
 import com.aplicativo.aplicativo.models.Eventos;
@@ -23,6 +24,14 @@ public class EventosController {
 	public String form(Eventos evento) {
 		er.save(evento);// sava no datavbase
 		return "redirect:/cadastrarEvento";//redireciona para o evento do GET
+	}
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv= new ModelAndView("index");//
+		Iterable<Eventos> eventos= er.findAll();//Interable porque vai ser uma lista de elementos
+		mv.addObject("evento",eventos);
+		
+		return mv;	
 	}
 
 }
